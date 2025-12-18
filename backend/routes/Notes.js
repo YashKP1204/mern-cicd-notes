3de2b6b0889a9e2aee12ba4742362dca8bc54d98
YashKP1204 <yashprajapati121204@gmail.com>
2025-12-18 22:51:46 +0530

    naming conflict

--- backend/routes/Notes.js
+++ backend/routes/Notes.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { protect } = require('../middleware/auth');
+const { protect } = require("../middleware/auth");
 const {
   getNotes,
   getNote,
@@ -9,22 +9,22 @@ const {
   deleteNote,
   toggleFavorite,
   toggleArchive,
-  getStats
-} = require('../controllers/notesController');
+  getStats,
+} = require("../controllers/notesController");
 
 // All routes are protected (require authentication)
 router.use(protect);
 
 // Stats route (must be before /:id routes)
-router.get('/stats', getStats);
+router.get("/stats", getStats);
 
 // CRUD routes
-router.route('/').get(getNotes).post(createNote);
+router.route("/").get(getNotes).post(createNote);
 
-router.route('/:id').get(getNote).put(updateNote).delete(deleteNote);
+router.route("/:id").get(getNote).put(updateNote).delete(deleteNote);
 
 // Special action routes
-router.put('/:id/favorite', toggleFavorite);
-router.put('/:id/archive', toggleArchive);
+router.put("/:id/favorite", toggleFavorite);
+router.put("/:id/archive", toggleArchive);
 
 module.exports = router;
