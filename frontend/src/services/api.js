7b536daf4ed7d1d04aa0c6ebc386b3396a73bd30
YashKP1204 <yashprajapati121204@gmail.com>
2026-08-02 09:47:38 +0530

    checking the react api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
+console.log("API_URL:", process.env.REACT_APP_API_URL);
+
 // Create axios instance
 const api = axios.create({
   baseURL: API_URL,
